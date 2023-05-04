97c32e1c17d40b1b6cf5fa9d9f89d28ef01f3aeb
Andy Haynes <andy@near.org>
2023-05-04 11:17:48 -0700

    clean up unused service code

--- packages/proxy/src/app.controller.ts
+++ packages/proxy/src/app.controller.ts
@@ -1,23 +1,9 @@
 import { Controller, Get, Param } from '@nestjs/common';
 
-import { compileWidget, transpileWidget } from "./transpiler";
+import { fetchWidgetSource, transpileWidget } from './transpiler';
 
 @Controller()
 export class AppController {
-  @Get('source/:widgetId')
-  getHardcodedWidget(@Param('widgetId') widgetId: string) {
-    return {
-      source: `
-        import { h } from 'https://esm.sh/preact';
-        const node = h('div', { className: 'iframe-initialized' }, '${widgetId}');
-        window.parent.postMessage(
-          JSON.stringify({ type: 'IFRAME_RENDER', id: '${widgetId}', node }),
-          '*'
-        );
-      `,
-    };
-  }
-
   @Get('widget/:accountId/widget/:widgetId')
   async getNSWidget(@Param('accountId') accountId: string, @Param('widgetId') widgetId: string) {
     const blacklist = [
@@ -30,17 +16,12 @@ export class AppController {
       };
     }
     return {
-      source: await compileWidget(`${accountId}/widget/${widgetId}`),
+      source: await transpileWidget(`${accountId}/widget/${widgetId}`),
     };
   }
 
-  @Get('raw/:accountId/widget/:widgetId')
-  async getNSWidgetRaw(@Param('accountId') accountId: string, @Param('widgetId') widgetId: string) {
-    return await compileWidget(`${accountId}/widget/${widgetId}`);
-  }
-
-  @Get('transpiled/:accountId/widget/:widgetId')
+  @Get('source/:accountId/widget/:widgetId')
   async getNSWidgetSource(@Param('accountId') accountId: string, @Param('widgetId') widgetId: string) {
-    return await transpileWidget(`${accountId}/widget/${widgetId}`);
+    return await fetchWidgetSource(`${accountId}/widget/${widgetId}`);
   }
 }

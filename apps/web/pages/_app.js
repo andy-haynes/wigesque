f49d5eb7afa0f95bc280ddf272cb1632daf927ec
Andy Haynes <andy@near.org>
2023-06-01 15:51:06 -0700

    use bootstrap

--- apps/web/pages/_app.js
+++ apps/web/pages/_app.js
@@ -1,6 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles.css'
+
+import { useEffect } from 'react';
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
+
   return <Component {...pageProps} />
 }
